b4c4ba7a9a48ae70c9301788b22da34bb6be513e
githkdh <kkimluv@gmail.com>
2013-11-27 23:05:20 +0900

    갱신

--- HTML/postcode-search/res/postcode.js
+++ HTML/postcode-search/res/postcode.js
@@ -3,6 +3,13 @@
 var results = document.getElementById("results");
 var postcodeSearchForm = document.getElementById('postcodeSearchForm');
 postcodeSearchForm.onsubmit = function() {
-	
+	xhr({
+		type: "GET", 
+		url: "http://10.52.41.91:9090/postcode/search?address=영등포&addressType=PLAIN&_pageItemSize=5&_pageNumber=0", 
+		success: function(data) {
+			results.innerHTML = data;
+			console.log(data);
+		}
+	});
 	return false; 
 };
