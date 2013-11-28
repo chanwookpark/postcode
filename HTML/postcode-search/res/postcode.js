4bc449b8f40d89016466819bdec2148e363ab614
githkdh <kkimluv@gmail.com>
2013-11-28 13:48:54 +0900

    갱신...

--- HTML/postcode-search/res/postcode.js
+++ HTML/postcode-search/res/postcode.js
@@ -1,15 +1,50 @@
 
-// 비동기로 조회된 값을 포함하는 영역
-var results = document.getElementById("results");
-var postcodeSearchForm = document.getElementById('postcodeSearchForm');
+var doc = document; 
+var results = doc.getElementById("results");
+var postcodeSearchForm = doc.getElementById('postcodeSearchForm');
 postcodeSearchForm.onsubmit = function() {
+	runXhr(); 
+	return false; 
+};
+
+/*
+var pagingNav = doc.getElementById("paging-nav");
+var pagingNavLink = pagingNav.getElementsByTagName("A");
+for(var i = 0; i < pagingNavLink.length; i++) {
+	pagingNavLink[i].onclick = function() {
+		runXhr();
+		return false; 
+	};
+}
+*/
+
+var formModel = {
+	"addressType": "PLAIN"
+};
+var addressTypeA = doc.getElementById("addressTypeA");
+var addressTypeB = doc.getElementById("addressTypeB");
+var address = doc.getElementById("address");
+
+addressTypeA.onclick = addressTypeEvt;
+addressTypeB.onclick = addressTypeEvt;
+
+function addressTypeEvt(e) {
+	formModel[this.name] = this.value;
+}
+
+
+function runXhr() {
+	var URLtemplate = "{action}?addressType={addressType}&address={address}"
+		.replace("{action}", postcodeSearchForm.action)
+		.replace("{addressType}", formModel.addressType)
+		.replace("{address}", address.value);
+
 	xhr({
 		type: "GET", 
-		url: "http://10.52.41.91:9090/postcode/search?address=영등포&addressType=PLAIN&_pageItemSize=5&_pageNumber=0", 
+		url: URLtemplate, 
 		success: function(data) {
 			results.innerHTML = data;
 			console.log(data);
 		}
 	});
-	return false; 
-};
+}
